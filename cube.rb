cb83493fcc342fa7d1f8826bda219d0e90fd586d
Colby Rabideau <crabideau5691@gmail.com>
2012-10-16 15:12:00 -0400

    stubbed out some methods for the cube module

---
+++ cube.rb
@@ -0,0 +1,30 @@
+=begin
+Goal State
+
+RRRRRRRRRGGGYYYBBBGGGYYYBBBGGGYYYBBBOOOOOOOOOWWWWWWWWW
+
+or
+
+RRR
+RRR
+RRR
+GGGYYYBBB
+GGGYYYBBB
+GGGYYYBBB
+OOO
+OOO
+OOO
+WWW
+WWW
+WWW
+=end
+
+module Cube
+
+  def self.corners(cube)
+  end
+
+  def self.edges(cube)
+  end
+
+end
