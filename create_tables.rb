4038f2af9f7c08d34990630eebacce4fd7ed15b4
Dan Miller <dan@danielsmiller.com>
2012-10-16 13:09:56 -0400

    Two different ways to represent the state of the cube.

--- create_tables.rb
+++ create_tables.rb
@@ -1,9 +1,61 @@
 #!/usr/bin/ruby
 
+require "awesome_print"
+
+#target_pattern = [
+  #[:r, :r, :r],
+  #[:r, :r, :r],
+  #[:g, :g, :g, :y, :y, :y, :b, :b, :b],
+  #[:g, :g, :g, :y, :y, :y, :b, :b, :b],
+  #[:g, :g, :g, :y, :y, :y, :b, :b, :b],
+  #[:o, :o, :o],
+  #[:o, :o, :o],
+  #[:o, :o, :o],
+  #[:w, :w, :w],
+  #[:w, :w, :w],
+  #[:w, :w, :w]
+#]
+
+target_pattern = [
+  [
+    [:r, :r, :r],
+    [:r, :r, :r],
+    [:r, :r, :r]
+  ],
+
+  [
+    [:g, :g, :g],
+    [:g, :g, :g],
+    [:g, :g, :g]
+  ],
+
+  [
+    [:y, :y, :y],
+    [:y, :y, :y],
+    [:y, :y, :y]
+  ],
+
+  [
+    [:b, :b, :b],
+    [:b, :b, :b],
+    [:b, :b, :b]
+  ],
+
+  [
+    [:o, :o, :o],
+    [:o, :o, :o],
+    [:o, :o, :o]
+  ],
+
+  [
+    [:w, :w, :w],
+    [:w, :w, :w],
+    [:w, :w, :w]
+  ]
+]
+
+
 if __FILE__ == $0
   puts "Starting mah script"
-end
-
-def get_heuristic state
-
+  ap(target_pattern)
 end
