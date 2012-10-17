fb48f5fc99f88deb40ce6325b541a38d9e52adce
Colby Rabideau <crabideau5691@gmail.com>
2012-10-17 15:51:28 -0400

    basic encoder/decoder for corners

---
+++ encoder.rb
@@ -0,0 +1,18 @@
+module Encoder
+
+  CONVERSION = ["GRW", "BRW", "GRY", "BRY", "GOY", "BOY", "GOW", "BOW"]
+
+  def self.encode(corners)
+    Integer(corners.map { |corner| CONVERSION.index(corner).to_s }.join, 8)
+  end
+
+  def self.decode(key)
+    s = key.to_s(8)
+    while s.size < 8
+      s = '0' + s
+    end
+    s.split("").map { |corner| CONVERSION[Integer(corner)] }
+  end
+
+
+end
