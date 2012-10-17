662e701f7be3ae67cddc469c9fc58d591e448749
Colby Rabideau <crabideau5691@gmail.com>
2012-10-17 18:11:49 -0400

    create heuristic tables

--- create_tables.rb
+++ create_tables.rb
@@ -1,5 +1,6 @@
 require './cube.rb'
-require './heuristic.rb'
+require './encoder.rb'
+# require './heuristic.rb'
 require 'csv'
 
 initial_state = "RRRRRRRRRGGGYYYBBBGGGYYYBBBGGGYYYBBBOOOOOOOOOWWWWWWWWW"
@@ -7,48 +8,29 @@ initial_state = "RRRRRRRRRGGGYYYBBBGGGYYYBBBGGGYYYBBBOOOOOOOOOWWWWWWWWW"
 if __FILE__ == $0
   puts "Starting mah script"
   #new_state = Cube.rotateClockwise(initial_state, "Y")
-  
-  depth = 0
 
-  CSV.open("corners.csv", "wb") do |csv|
-    csv << Cube.corners(initial_state).unshift(depth)
-    stack = Array.new
-    stack.push(initial_state)
-    depth = depth + 1
-    temp_stack = Array.new
+  previous = Array[initial_state]
+  depth = 1
+  temp = Array[]
 
-    while depth < 2
-      Cube::FACES.keys.each_with_index {|face, index|
-        #puts depth.to_s + ' ' + face.to_s
-        #csv << Cube.corners(Cube.rotateClockwise(initial_state, face)).unshift(depth)
-        #csv << Cube.corners(Cube.rotateCounterClockwise(initial_state, face)).unshift(depth)
-        #csv << Cube.corners(Cube.rotate180(initial_state, face)).unshift(depth)
+  while depth < 4
 
-        while !stack.empty?
-          state = stack.pop
+    previous.each do |current|
 
-          clockwised_state = Cube.rotateClockwise(state, face)
-          temp_stack.push(clockwised_state)
-          counterclockwised_state = Cube.rotateCounterClockwise(state, face)
-          temp_stack.push(counterclockwised_state)
-          flipped_state = Cube.rotate180(state, face)
-          temp_stack.push(flipped_state)
-
-          csv << Cube.corners(clockwised_state).unshift(depth)
-          csv << Cube.corners(counterclockwised_state).unshift(depth)
-          csv << Cube.corners(flipped_state).unshift(depth)
-
+      Cube::FACES.keys.each do |face|
+        temp.push(Cube.rotate180(current, face))
+        temp.push(Cube.rotateClockwise(current, face))
+        temp.push(Cube.rotateCounterClockwise(current, face))
+        temp[-3, 3].each do |state|
+          puts CornerEncoder.encode(Cube.corners(state)) 
         end
 
-        puts "Escaped!"
-
-        stack = temp_stack
-
-      }
-      
-      depth = depth + 1
+      end
 
     end
+    previous = temp
+    temp = []
+    depth += 1
   end
 
 end
